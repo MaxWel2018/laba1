fd2c712b9b846766f56e4155abc4a426ccec40ce
Maksym <18maxim1998@gmail.com>
2019-09-10 20:12:20 +0300

    Selection Sort

--- src/main/java/com/maksym/laba1/utility/UtilityForSorting.java
+++ src/main/java/com/maksym/laba1/utility/UtilityForSorting.java
@@ -1,9 +1,10 @@
 package com.maksym.laba1.utility;
 
 public class UtilityForSorting {
-        public static void swap(int[] arrayForSorting, int j) {
+        public static void swap(int[] arrayForSorting, int j,int k) {
             int temp = arrayForSorting[j];
-            arrayForSorting[j] = arrayForSorting[j + 1];
-            arrayForSorting[j + 1] = temp;
+            arrayForSorting[j] = arrayForSorting[k];
+            arrayForSorting[k] = temp;
         }
+
 }
